7efdfaec7742fc4c1f8d93ee542e444f64e4c326
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-07 05:45:25 +0300

    converted list to tuple

--- 102-type_checking.py
+++ 102-type_checking.py
@@ -5,10 +5,10 @@ from typing import Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    zoomed_in: Tuple = [
+    zoomed_in: Tuple = tuple([
         item for item in lst
         for i in range(factor)
-    ]
+    ])
     return zoomed_in
 
 
