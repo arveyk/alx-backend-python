9f05061fecf8b2b53022ffa5891ad0027b49c942
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-07 04:57:27 +0300

    Duck typing an iterable object

---
+++ 9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module to explore duck typing
+"""
+from collections.abc import Sequence
+
+def element_length(lst: list[int]) -> tuple(int, int):
+    """Function that return the length of an iterable
+    Args:
+        lst: list whose length is to be evaluated
+    Returns: the list length
+    """
+    return [(i, len(i)) for i  in lst ]
