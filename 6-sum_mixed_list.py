728e77175f9840bf450b1ff49b122566cbc3315f
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-07 04:55:16 +0300

    Complex types - mixed lisr

---
+++ 6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module to define function for summing a list of mixed number types
+"""
+
+def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+    """Function that sums a list of integers and/or floats
+    Args:
+        mxd_lst: list containing numbers to be summed
+    Returns: the sum of the numbers"""
+    total = 0.00
+    for item in mxd_lst:
+        total += float(item)
+    return total
