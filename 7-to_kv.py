4b18b9569adf331891a0794ea1b50eb2bd5a8edc
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-07 04:56:06 +0300

    Complex types-string and int/float to tuple

---
+++ 7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module to define function for returning a tuple made up of 
+the arguments it is supplied
+"""
+
+def to_kv(k: str, v: int | float) -> tuple:
+    """Function that returns a tuple consisting of the arguments it is
+    supplied
+    Args:
+       k: the 'key' of the tuple
+       v: the number to be supplied
+    Returns: a tupple"""
+
+    tup = (k, v)
+    return tup
