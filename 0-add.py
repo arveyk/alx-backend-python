99990da4d1ff0cb4a2a4c58c24c34e9c4bb20b6d
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-07 04:50:38 +0300

    Basic annotations -add

---
+++ 0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Script to add two numbers
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Function two add two numbers
+    Args:
+        a: first number
+        b: second number
+    Returns: sum of the two numbers"""
+    return a + b
