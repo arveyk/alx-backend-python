db37c9fcf4672ddb49199b158f3e487cc7d1d1ca
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-07 05:06:27 +0300

    Basic annotations - to string

---
+++ 3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Module to define a to string function
+"""
+
+
+def to_str(n: float) -> str:
+    """ Function to return the string representation of a decimal number
+    Args:
+        n: the number to be evaluated
+    Returns: the string representation of str"""
+
+    return repr("{n}")
