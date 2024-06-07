ed8850d6f176f101748efcf1189d6e5bfd14c4ae
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-07 04:51:09 +0300

    basic annotations floor

---
+++ 2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module for floor function definition
+"""
+
+
+def floor(n: float) -> int:
+    """ Function that takes a float and floors it
+    Args:
+        n: the float to be floored
+    Returns: the floored result"""
+    import floor from math
+
+    return floor(n)
