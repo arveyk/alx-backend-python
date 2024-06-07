b010589e86f41ce23805d0c1c43bca4375ab1257
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-07 13:25:43 +0300

    Function Documented

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,6 +5,11 @@ from typing import Tuple
 
 
 def zoom_array(lst: list, factor: int = 2) -> Tuple:
+    """Function to zoom into a list by a given factor
+    Args:
+        lst: list of numbers to be zoomed
+        factor: number of times to zoom in
+    Returns: tuple of the factored list"""
     zoomed_in: Tuple = tuple([
         item for item in lst
         for i in range(factor)
