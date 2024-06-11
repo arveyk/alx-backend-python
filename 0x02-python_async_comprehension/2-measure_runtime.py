6ebfd45065f804805f5420d244b4ce5bf814fbcc
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-11 15:40:34 +0300

    Runtime for 4 parallel comprehensions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for function that measures runtime
+"""
+import time
+import asyncio
+
+asyncCom = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    Function to measure runtime
+    Args:
+        None
+    Returns: the total amout of time it takes to execute an async function
+    """
+    start = time.perf_counter()
+    await asyncio.gather(asyncCom(), asyncCom(), asyncCom(), asyncCom())
+    elapsed_t = time.perf_counter() - start
+    return elapsed_t
