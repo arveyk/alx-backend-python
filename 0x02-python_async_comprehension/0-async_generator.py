c45d6eade7bed5cfc337ad915a5e5f325201d82c
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-11 15:43:16 +0300

    imports at top of file

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Module for creating async generator functions
 """
+import asyncio
+import random
 
 
 async def async_generator() -> float:
@@ -8,9 +10,6 @@ async def async_generator() -> float:
     Args : none
     Returns: yields some numbers
     """
-    import asyncio
-    import random
-
     for i in range(10):
         rand_number = random.uniform(0, 10)
         await asyncio.sleep(1)
