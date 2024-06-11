cab1940d64ca1e7e624b77007c26994be9d356f9
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-11 16:22:31 +0300

    Typing List

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,12 +3,13 @@
 """
 import asyncio
 import random
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """ Function to implement async comprehension
     Args:
         None
