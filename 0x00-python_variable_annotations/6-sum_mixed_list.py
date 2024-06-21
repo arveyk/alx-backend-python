5436d0375df4d9ec3106cf7cbc2a6f7b64a38585
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-21 19:45:06 +0300

    Union of 2 types

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Module to define function for summing a list of mixed number types
 """
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Function that sums a list of integers and/or floats
     Args:
         mxd_lst: list containing numbers to be summed
