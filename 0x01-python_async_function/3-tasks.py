d12f4d95711ec86dabc779fcb4b1c4ebaf170507
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-21 16:50:24 +0300

    Return type Awaitable

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,12 +2,13 @@
 """ Module for running tasks
 """
 import asyncio
-from typing import Awaitable
+from typing import Any
+from collections.abc import Awaitable
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable:
+def task_wait_random(max_delay: int) -> Awaitable[int]:
     """Task generating function
     Args:
         max_delay: maximum amount of time to delay
