0be10d86f3634b092294bf92bf912622f95929ee
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-21 22:53:31 +0300

    pycodestyle

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,7 +7,8 @@ from typing import Any, Union, Mapping, TypeVar
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """Function to return member of a dict at a given key
     Args:
         dct: dictionary
