7e6823ac9b993fc5845ec2ed1acda56e6ce5197a
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-21 19:36:47 +0300

    from typing List imported

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Module to define an annotated function
 """
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Function to add a list of floats
     Args:
         input_list: a list of numbers to be summed
