45767ef7b394db47c101fc7917758a4e78907e60
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-21 20:27:24 +0300

    Use of main function to test

---
+++ 0x00-python_variable_annotations/8-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 8-main.py
+"""
+
+make_multiplier = __import__('8-make_multiplier').make_multiplier
+print(make_multiplier.__annotations__)
+fun = make_multiplier(2.22)
+print("{}".format(fun(2.22)))
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 """Module to define a function that returns a function multiplier
 """
-from colletions.abc import Callable
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[[float] float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Function that takes an argument and return a function that
     multiplies that number by a fixed multiplier
     Args:
         multiplier: a number to multiply by
     Return: the function to do the multiplication
     """
-    def mul(mul_plier: float):
-        """Function that multiplies an argument by a given argument
-        """
-        return mul_plier * 1.4239
 
-    return mul(multiplier)
+    def mul(num: float) -> float:
+        """Function that mutiplies
+        Args:
+            num: number to multiply
+        Returns: result
+        """
+        return multiplier * num
+
+    return mul
