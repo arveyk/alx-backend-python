17ec33729608b7581faaca7782dc7c42d73a9f7a
Harvey W. Kis <harveykisiangani@gmail.com>
2024-06-21 16:53:23 +0300

    use of typing module in place of collection.abc

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,12 +3,12 @@
 """
 import asyncio
 from typing import Any
-from collections.abc import Awaitable
+from typing import Awaitable
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable[int]:
+def task_wait_random(max_delay: int) -> Awaitable[Any]:
     """Task generating function
     Args:
         max_delay: maximum amount of time to delay
