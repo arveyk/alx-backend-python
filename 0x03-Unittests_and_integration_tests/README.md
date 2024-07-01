07e5d14db4fe2b1fb595aff2766d6801ef801a90
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-01 17:04:35 +0300

    Test without parameterized

--- 0x03-Unittests_and_integration_tests/README.md
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -2,3 +2,5 @@
 
 Here we are looking at integrating tests with codes making testing as seamless 
 as possible so that the are minimal changes to the code and the test
+Unit test are for individual modules or pieces of code while Integration test 
+are for units of a program combined as a group
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Test Module
+"""
+from parameterized import parameterized
+import unittest
+
+
+access_nested_map = __import__("utils")
+class TestAccessNestedMap(unittest.TestCase):
+    """Class for testing the method access_nested_map
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a", )),
+        ({"a": {"b": 2}}, ("a", )),
+        ({"a": {"b": 2}}, ("a", "b"))
+        ])
+    def test_access_nested_map(self, nested_map, path):
+        """Test the method to assert that it functions as req
+        Args: None
+        Returns: None
+        """
+        self.assertEqual(nested_map, path)
