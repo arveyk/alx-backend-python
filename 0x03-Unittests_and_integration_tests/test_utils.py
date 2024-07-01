da8d70bbc87265657d2434980d147cb9891af104
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-01 17:53:53 +0300

    called the function being tested

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,12 +10,12 @@ class TestAccessNestedMap(unittest.TestCase):
     """Class for testing the method access_nested_map
     """
     @parameterized([
-        ({"a": 1}, ("a", )),
-        ({"a": {"b": 2}}, ("a", "b")),
+        ({"a": 1}, ("a", ), 1),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path):
+    def test_access_nested_map(self, nested_map, path, expect):
         """Test the method to assert that it functions as req
         Args: None
         Returns: None
         """
-        self.assertEqual(nested_map, path)
+        self.assertEqual(access_nested_map(nested_map, path), expect)
