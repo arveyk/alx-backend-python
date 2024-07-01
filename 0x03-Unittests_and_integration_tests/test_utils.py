171387e1c4bb4498fbd6452aa8681bb598245e58
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-01 19:31:54 +0300

    raise exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,7 +16,22 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expect):
         """Test the method to assert that it functions as req
-        Args: None
+        Args:
+            nested_map: map to evaluate
+            path: keys for accessing nested map
+            except: expected result
         Returns: None
         """
         self.assertEqual(access_nested_map(nested_map, path), expect)
+
+    @parameterized.expand([
+        ({}, ("a", )),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Raise exception for a particular error
+        Args:
+            wait and see
+        Returns: Just wait
+        """
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
