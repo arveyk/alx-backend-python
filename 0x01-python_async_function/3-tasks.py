10bf84205e41ad9fb45c0ee15cfab1363fe10375
arveyk <harveykisiangani@gmail.com>
2024-09-11 20:52:39 +0300

    Type annotation of return value

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,13 +2,13 @@
 """ Module for running tasks
 """
 import asyncio
-from typing import Any
+# from typing import Any
 from typing import Awaitable
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable[Any]:
+def task_wait_random(max_delay: int) -> Awaitable[int]:
     """Task generating function
     Args:
         max_delay: maximum amount of time to delay
